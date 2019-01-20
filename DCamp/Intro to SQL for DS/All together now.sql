c3f67a6e641cd20f1157f47cf62f2168b7266258
SMatsu <matsushin51@gmail.com>
2019-01-21 06:12:13 +0900

    Create All together now.sql

---
+++ DCamp/Intro to SQL for DS/All together now.sql
@@ -0,0 +1,8 @@
+SELECT release_year, 
+       AVG(budget) AS avg_budget,
+       AVG(gross) AS avg_gross
+FROM films
+WHERE release_year > 1990
+GROUP BY release_year
+HAVING AVG(budget) > 60000000
+ORDER BY AVG(gross) DESC;
